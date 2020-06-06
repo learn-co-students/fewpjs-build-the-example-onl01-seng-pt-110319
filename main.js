b5986b171a1140c281a9c53093b705ed24c7da76
Jordan <jordanleelong5@gmail.com>
2020-06-05 23:49:01 -0700

    got the badStatus working

--- main.js
+++ main.js
@@ -3,6 +3,77 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+function hideModal() {
+  let hidden = document.getElementById('modal');
+  hidden.classList.add("hidden");
+
+}
+
+hideModal();
+
+function likeBtn() {
+  let emptyLike = document.querySelectorAll('.like')
+  emptyLike.forEach(likeButtons => {
+    likeButtons.addEventListener("click", function(event) {
+      mimicServerCall("fakeUrl")
+      .then(function(response){
+      // response => response.json()
+      goodStatus(response);
+      // console.log(response)
+      })
+      .catch(function(error) {
+        badStatus(error);
+      })
+    })
+  })
+}
+likeBtn();
+
+function goodStatus(response) {
+  let likeBtn = document.querySelectorAll('.like')
+  //HERE
+
+  console.log(FULL_HEART)
+  console.log(response)
+}
+
+function badStatus(error) {
+  let hidden = document.getElementById('modal')
+  hidden.classList.remove("hidden")
+  hidden.innerHTML = error;
+
+    setTimeout(function() {
+      hidden.classList.add("hidden");
+
+    }, 1000)
+  }
+  // setTimeout(function() {
+
+
+  // }, 1000)
+
+// function checkStatus (res) {
+// if (res.status >= 200 && res.status < 300) {
+//   hideModal();
+//   console.log(res)
+// } else {
+
+//     let hidden = document.getElementById('modal')
+//     hidden.innerHTML = res;
+//     hidden.classList.remove("hidden")
+
+// }
+// }
+
+
+
+
+
+
+
+
+
+
 
 
 
