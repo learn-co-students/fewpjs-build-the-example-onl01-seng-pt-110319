04177bb809639a1dfd6bc43ecec801abdc8bf662
Jordan <jordanleelong5@gmail.com>
2020-06-06 22:48:10 -0700

    finished regular lab

--- main.js
+++ main.js
@@ -1,6 +1,86 @@
-// Defining text characters for the empty and full hearts for you to use later.
+// // Defining text characters for the empty and full hearts for you to use later.
+// const EMPTY_HEART = '♡'
+// const FULL_HEART = '♥'
+
+// // Your JavaScript code goes here!
+// function hideModal() {
+//   let hidden = document.getElementById('modal');
+//   hidden.classList.add("hidden");
+
+// }
+
+
+// function heartBtn() {
+//   let emptyLikes = document.querySelectorAll('.like-glyph')
+//   emptyLikes.forEach(likeButton => {
+//     likeButton.addEventListener("click", function(event) {
+//       mimicServerCall("fakeUrl", {article_id: this.parentNode.parentNode.parentNode.id
+//      }) 
+//       .then(function(response){
+//         goodStatus(likeButton);
+//       })
+//       .catch(function(error) {
+//         console.log(error)
+//         badStatus(error);
+//       })
+//     })
+//   })
+// }
+
+// function goodStatus(response) {
+//   let article = document.querySelector("[id='"+ response.article_id + "']")
+  
+//   let heart = article.querySelector('.like') // searches for a class
+//   console.log(article)
+//   // heart.classList.remove('like')
+//   // heart.classList.add('activated-heart')
+//   // heart.innerText = FULL_HEART
+
+  
+//  }
+
+// function badStatus(error) {
+//   let hidden = document.getElementById('modal')
+//   hidden.classList.remove("hidden")
+//   hidden.innerHTML = error;
+
+//     setTimeout(function() {
+//       hidden.classList.add("hidden");
+
+//     }, 5000)
+//   }
+
+
+// document.addEventListener("DOMContentLoaded", event => {
+//   heartBtn();
+//   hideModal();
+// })
+
+
+  
+
+// //------------------------------------------------------------------------------
+// // Ignore after this point. Used only for demo purposes
+// //------------------------------------------------------------------------------
+
+// function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+//   return new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//       let isRandomFailure = Math.random() < .2
+//       if (isRandomFailure) {
+//         reject(config);
+//       } else {
+//         resolve(config);
+//       }
+//     }, 300);
+//   });
+// }
+
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+
+
+
 
 // Your JavaScript code goes here!
 function hideModal() {
@@ -9,17 +89,15 @@ function hideModal() {
 
 }
 
-hideModal();
+
 
 function likeBtn() {
-  let emptyLike = document.querySelectorAll('.like')
-  emptyLike.forEach(likeButtons => {
-    likeButtons.addEventListener("click", function(event) {
+  let emptyLikes = document.querySelectorAll('.like-glyph')
+  emptyLikes.forEach(likeButton => {
+    likeButton.addEventListener("click", function(event) {
       mimicServerCall("fakeUrl")
       .then(function(response){
-      // response => response.json()
-      goodStatus(response);
-      // console.log(response)
+      goodStatus(likeButton);
       })
       .catch(function(error) {
         badStatus(error);
@@ -27,14 +105,21 @@ function likeBtn() {
     })
   })
 }
-likeBtn();
 
-function goodStatus(response) {
-  let likeBtn = document.querySelectorAll('.like')
-  //HERE
 
-  console.log(FULL_HEART)
-  console.log(response)
+function goodStatus(heart) {
+
+  // console.log(heart.innerText)
+
+  if (heart.innerText === EMPTY_HEART) {
+    heart.classList.add('activated-heart')
+    heart.innerText = FULL_HEART
+  } else {
+    heart.classList.remove('activated-heart')
+    heart.innerText = EMPTY_HEART
+
+  }
+ 
 }
 
 function badStatus(error) {
@@ -45,35 +130,16 @@ function badStatus(error) {
     setTimeout(function() {
       hidden.classList.add("hidden");
 
-    }, 1000)
+    }, 3000)
   }
-  // setTimeout(function() {
 
 
-  // }, 1000)
+document.addEventListener("DOMContentLoaded", event => {
+  likeBtn();
+  hideModal();
+})
 
-// function checkStatus (res) {
-// if (res.status >= 200 && res.status < 300) {
-//   hideModal();
-//   console.log(res)
-// } else {
-
-//     let hidden = document.getElementById('modal')
-//     hidden.innerHTML = res;
-//     hidden.classList.remove("hidden")
-
-// }
-// }
-
-
-
-
-
-
-
-
-
-
+ 
 
 
 
