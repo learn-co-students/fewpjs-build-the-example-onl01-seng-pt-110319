3b620bc0c6008c932e260477ea27c62a316ee57a
Aida Abdikulova <aabdikul@Aidas-MBP.fios-router.home>
2020-06-10 02:06:31 -0400

    Done.

--- main.js
+++ main.js
@@ -4,8 +4,41 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let modal = document.getElementById("modal")
 
+let likes = document.getElementsByTagName("li")
+const jsLike = likes[0]
+const rubyLike = likes[1]
+const pythonLike = likes[2]
+const javaLike = likes[3]
+const elixrLike = likes[4]
 
+document.addEventListener("DOMContentLoaded", function() {
+  heart()
+  setTimeout(modalChange, 5000)
+})
+
+function modalChange () {
+  modal.hidden = true
+}
+
+function heart() {
+  jsLike.addEventListener("click", function() {
+    jsLike.innerHTML = "Like!" + " " + FULL_HEART
+  })
+  rubyLike.addEventListener("click", function() {
+    rubyLike.innerHTML = "Like!" + " " + FULL_HEART
+  })
+  pythonLike.addEventListener("click", function() {
+    pythonLike.innerHTML = "Like!" + " " + FULL_HEART
+  })
+  javaLike.addEventListener("click", function() {
+    javaLike.innerHTML = "Like!" + " " + FULL_HEART
+  })
+  elixrLike.addEventListener("click", function() {
+    elixrLike.innerHTML = "Like!" + " " + FULL_HEART
+  })
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
