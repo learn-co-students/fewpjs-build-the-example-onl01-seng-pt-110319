a1d6a804772e5caaf730f6866c2fce5ce6ef591e
Alouiscious Media <info@reneesoul.com>
2020-06-18 13:37:07 -0700

    Done.

--- main.js
+++ main.js
@@ -1,30 +1,49 @@
 // Defining text characters for the empty and full hearts for you to use later.
+let modal = document.getElementById("modal")
+modal.setAttribute('class', 'hidden')
 
- let modal = document.getElementById("modal").className="modal"
-  document.querySelector(".modal").hidden = true
+console.log('whatisit', modal)
 
-  const EMPTY_HEART = '♡'
-  const FULL_HEART = '♥'
-  // oneHeart.onclick == false
+const EMPTY_HEART = '♡'
+const FULL_HEART = '♥'
+// oneHeart.onclick == false
 
+let glyphStates = {
+  '♡': '♥',
+  '♥': '♡'
+}
+
+let colorStates = {
+  "red" : "",
+  "": "red"
+}
+
+// let allHearts = document.querySelectorAll("span.like-glyph");
+let allHearts = document.querySelectorAll(".like");
   
-    let allHearts = document.querySelectorAll("span.like-glyph");
-    for (let likeGlyph of allHearts) {
-      likeGlyph.addEventListener("click", function likeHeart(event) {
-        let oneHeart = event.target;
-        mimicServerCall()
-        .then(() => {
-          oneHeart.innerHTML = FULL_HEART;
-          oneHeart.style.color = "red";
-          // oneHeart.onclick == false;
-        })
-        .catch((toggleErr) => {
-          // document.innerHTML = document.querySelector(".modal")
-          let showErr = document.getElementById("modal").className="modal"
-          toggleErr(showErr);
-        })// USE? timeout -  window.setTimeout(window.alert, 5*1000
-      })
-    }
+for (let likeGlyph of allHearts) {
+  console.log('whatisit', likeGlyph)
+  likeGlyph.addEventListener("click", function(event) {
+    let oneHeart = event.target;
+    mimicServerCall()
+    .then(() => {
+      oneHeart.innerHTML = glyphStates[oneHeart.innerText];
+      oneHeart.style.color = colorStates[oneHeart.style.color];
+      // oneHeart.onclick == false;
+    })
+    .catch((error) => {
+      modal.removeAttribute('class', 'hidden')
+      console.log('whatisit', error)
+
+      // TIME OUT LOGIC
+      modal.innerText = error
+      setTimeout(function() {
+        modal.setAttribute('class', 'hidden')
+      }, 5*1000)
+    })
+  })
+}
+    
     
 
 //------------------------------------------------------------------------------
