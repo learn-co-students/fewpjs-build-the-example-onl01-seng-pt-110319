a586de8b46d4e1ff0b641487b8b73b17124a33a8
github username <github email address>
2021-01-12 22:20:40 -0500

    Done.

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // Defining text characters for the empty and full hearts for you to use later.
-let glyphStates = {
-  "♡": "♥",
-  "♥": "♡"
-};
+// let glyphStates = {
+//   "♡": "♥",
+//   "♥": "♡"
+// };
 
-let colorStates = {
-  "red" : "",
-  "": "red"
-};
+// let colorStates = {
+//   "red" : "",
+//   "": "red"
+// };
 
 const modal=document.querySelector("#modal")
 modal.setAttribute("class","hidden")
